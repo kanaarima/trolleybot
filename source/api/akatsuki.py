d629380b362981f1ef378eaf2fc04779d2a21e98
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-04-04 23:24:25 +0200

    add get user stats endpoint

--- source/api/akatsuki.py
+++ source/api/akatsuki.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Tuple
 from utils import Model
+from typing import *
 
 import requests
 import time
@@ -57,6 +57,54 @@ class Score(Model):
         return score
 
 
+class ClanInfo(Model):
+    id: int
+    name: str
+    tag: str
+    description: str
+    icon: str
+    owner: int
+    status: int
+
+class Stats(Model):
+    ranked_score: int
+    total_score: int
+    playcount: int
+    playtime: int
+    replays_watched: int
+    total_hits: int
+    level: float
+    accuracy: float
+    pp: int
+    global_leaderboard_rank: int
+    country_leaderboard_rank: int
+    max_combo: int
+
+class UserInfo(Model):
+    id: int
+    username: str
+    username_aka: str # Unused
+    registered_on: datetime
+    priviledges: int
+    latest_activity: datetime
+    country: str
+    stats: List[Dict[str, Stats]]
+    play_style: int
+    favourite_mode: int
+    clan: ClanInfo
+    followers: int
+    
+    def from_dict(dict):
+        user = UserInfo(**dict)
+        user.registered_on = datetime.strptime(user.registered_on, DATE_FORMAT)
+        user.latest_activity = datetime.strptime(user.latest_activity, DATE_FORMAT)
+        if 'stats' in dict:
+            user.stats = [{k: Stats(**v) for k, v in stats.items()} for stats in dict['stats']]
+        if 'clan' in dict:
+            user.clan = ClanInfo(**dict['clan'])
+        return user
+    
+
 class Akatsuki:
     
     def __init__(self, req_min=60):
@@ -69,15 +117,21 @@ class Akatsuki:
             time.sleep(delay - (time.time() - self.last_request))
         return requests.get(url)
     
-    def get_user_first_places(self, user_id: int, mode: int, relax: int) -> Tuple[List[Score], int]:
+    def get_user_first_places(self, user_id: int, mode: int, relax: int, count_only: bool) -> Tuple[List[Score], int]:
         first_places = list()
         page = 1
         while True:
             req = self.request(f"https://akatsuki.gg/api/v1/users/scores/first?mode={mode}&p={page}&l=100&rx={relax}&id={user_id}")
             if req.status_code != 200 or not req.json()['scores']:
                 break
+            if count_only:
+                return [], req.json()['total']
             first_places += [Score.from_dict(score) for score in req.json()['scores']]
             page += 1
         return first_places, req.json()['total']
 
-    
+    def get_user_stats(self, user_id: int) -> UserInfo:
+        req = self.request(f"https://akatsuki.gg/api/v1/users/full?id={user_id}")
+        if req.status_code != 200:
+            return None
+        return UserInfo.from_dict(req.json())
